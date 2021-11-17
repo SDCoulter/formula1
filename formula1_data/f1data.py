426fe2239d4a720fedd1cd7340f021d7cae1664c
Shaun Coulter <shaundcoulter@gmail.com>
2021-11-17 10:30:46 -0400

    add route for constructor standings

--- formula1_data/f1data.py
+++ formula1_data/f1data.py
@@ -4,7 +4,7 @@ Create the Blueprints to allow us to start displaying data.
 
 # Check which modules to import.
 from flask import (
-    Blueprint, redirect, render_template
+    Blueprint, render_template, request
 )
 
 from formula1_data.db import get_db
@@ -28,3 +28,44 @@ def index():
     """
 
     return render_template('f1data/index.html')#, tables=tables)
+
+
+# Route for constructor standings.
+@bp.route('/con_standings', methods=('GET', 'POST'))
+def con_standings():
+
+    # TODO: create default so data can be passed to the page before user selection.
+    # TODO: bring db creation here for default search.
+    data_match = ''
+
+    # Form sent with data to populate template.
+    if request.method == 'POST':
+        season_year = request.form['season_year']
+        round_no = request.form['round_no']
+
+    # Search database instance first for corresponding data.
+        db = get_db()
+        no_db_data = False
+
+        try:
+            data_match = db.execute(
+                "SELECT * FROM constructor_standings"
+                " WHERE season_year = ? AND round_no = ?"
+                " ORDER BY position"
+            ).fetchall()
+        except:
+            no_db_data = True
+
+        # Else find the data from the API.
+        if no_db_data:
+            data_match = ''
+            # TODO: parse the selection to create the url.
+            # TODO: use the moves.py function to collect the data and return a DataFrame
+            # TODO: store the data in the db.
+
+
+
+    # Pass the data back to the template to display.
+    return render_template('f1data/con_standings.html', data=data_match)
+
+    # Use graphs, etc, to display data depending on request from user. (html/js)
