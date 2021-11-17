006c73fafe8c24a02aad9a8b63e00903e22ad305
Shaun Coulter <shaundcoulter@gmail.com>
2021-11-17 09:27:05 -0400

    change schema to reflect new fields in con data;

--- formula1_data/schema.sql
+++ formula1_data/schema.sql
@@ -1,11 +1,22 @@
--- Create tables to hold the Formula 1 data.
+-- Table for data explorations.
+DROP TABLE IF EXISTS data_exps;
+CREATE TABLE data_exps (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT UNIQUE NOT NULL,
+  url TEXT UNIQUE NOT NULL
+);
 
--- 21-11-16 - For now we are focusing on Constructor championship data.
+-- Constructor Standings table.
 DROP TABLE IF EXISTS constructor_standings;
 -- 2020_constructorStandings.csv
 CREATE TABLE constructor_standings (
-  id INTEGER PRIMARY KEY,
-  constructor TEXT UNIQUE NOT NULL,
+  uid INTEGER UNIQUE NOT NULL,
+  position INTEGER,
+  constructor_name TEXT UNIQUE NOT NULL,
   points INTEGER NOT NULL,
-  wins INTEGER NOT NULL
+  wins INTEGER NOT NULL,
+  constructor_url TEXT NOT NULL,
+  constructor_nationality TEXT NOT NULL,
+  season_year INTEGER NOT NULL,
+  round_number INTEGER NOT NULL
 );
