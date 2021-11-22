9149f1d302387078c9d1941e01c53744c76d05ec
Shaun Coulter <shaundcoulter@gmail.com>
2021-11-22 13:39:06 -0400

    more testing for db and con_standings

---
+++ tests/test_db.py
@@ -0,0 +1,37 @@
+# Test database.
+import sqlite3, pytest
+from formula1_data.db import get_db
+
+
+def test_get_close_db(app):
+    # Test connection to the test database and connection is made.
+    with app.app_context():
+        db = get_db()
+        assert db is get_db()
+
+    # Try to execute a search on the database while unconnected.
+    with pytest.raises(sqlite3.ProgrammingError) as e:
+        db.execute('SELECT 1')
+
+    # Assert that we couldn't make the search because the connection is closed.
+    assert 'closed' in str(e.value)
+
+
+# Test initialization process of db.
+def test_init_db_command(runner, monkeypatch):
+    # Create object to record the initialization.
+    class Recorder(object):
+        called = False
+
+    # Fake function to change the status of the Recorder class.
+    def fake_init_db():
+        Recorder.called = True
+
+    # Instead of running init_db, confirm it was called by running fake_init_db.
+    monkeypatch.setattr('formula1_data.db.init_db', fake_init_db)
+    # Run the argument on the command line.
+    result = runner.invoke(args=['init-db'])
+    # Output will match if db initialized with no issue.
+    assert 'Database Initialized.' in result.output
+    # Check function was called.
+    assert Recorder.called
---
+++ tests/test_f1data.py
@@ -0,0 +1,38 @@
+import pytest
+from formula1_data.db import get_db
+
+
+# Test the index route.
+def test_index(client, app):
+    response = client.get('/')
+    # Check page renders as expected.
+    assert b"Formula 1" in response.data
+    assert b"GitHub Repo" in response.data
+    assert b"Constructor Standings Analysis" in response.data
+
+    # Check data.sql values are in database.
+    with app.app_context():
+        db = get_db()
+        res = db.execute('SELECT COUNT(id) FROM data_exps WHERE id = 8000').fetchone()[0]
+        assert res == 1
+
+
+# Test con_standings.
+def test_con_standings(client, app):
+    response = client.get('/con_standings')
+    # Check page.
+    assert b"Constructor Standings" in response.data
+    assert b"Constructor Name" in response.data
+    assert b"2021" in response.data
+
+    # Pass a post method to con_standings.
+    response = client.post('/con_standings', data={'season_year': 2017, 'round_no': '5 - Spanish Grand Prix'})
+    assert b"5" in response.data
+    assert b"Spanish" in response.data
+    assert b"2017" in response.data
+
+    with app.app_context():
+        db = get_db()
+        # Search for the posted data in the db.
+        res = db.execute('SELECT * FROM constructor_standings WHERE season_year == 2017').fetchone()
+        assert res['constructor_name'] == 'Mercedes'
