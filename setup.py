1159ea61f9cb33720290fd11c50941c06d053017
Shaun Coulter <shaundcoulter@gmail.com>
2021-11-22 10:53:58 -0400

    correct errors to make app installable.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 Create setup file to make the app installable.
 """
 
-from setuptools import fined_packages, setup
+from setuptools import find_packages, setup
 
 setup(
-    name='formula1-data'
-    version='1.0.0'
-    packages=fined_packages()
-    include_package_data=True
+    name='formula1_data',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
     zip_safe=False,
     install_requires=[
         'flask',
